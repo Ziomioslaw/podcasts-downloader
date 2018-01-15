949484339d3234af0f871f322d67f6c8c9207e3b
Ziomioslaw <ziomioslaw@wp.pl>
2018-01-15 18:56:34 +0100

    Adjust FeedReader for extracting all mp3 link

--- mp3fromrss/FeedReader.py
+++ mp3fromrss/FeedReader.py
@@ -10,15 +10,7 @@ class FeedReader():
         items = self.feedSource.run()
 
         for item in items:
-            episode = self._parseItem(item)
-            if episode != None:
-                yield episode
-
-    def _parseItem(self, item):
-        links = item['links']
-
-        if len(links) > 0:
             publishedDate = datetime.strptime(item['published'][:-6], '%a, %d %b %Y %H:%M:%S')
-            return Episode(publishedDate, item['links'][0]['href'])
-
-        return None
+            for link in item['links']:
+                if 'mp3' in link['href']:
+                    yield Episode(publishedDate, link['href'])
